fb918f620ab4796147c7e8abb391bc3999041da0
Ed Crossett <46727753+ecrossett@users.noreply.github.com>
2019-02-26 01:34:27 -0500

    Update and rename approximate_e to approximate_e.m

--- approximate_e.m
+++ approximate_e.m
@@ -1,8 +1,7 @@
 function [e, kmax] = approximate_e(delta)
-
 % This function approximates the value of e using user input for
 % tolerance threshold, delta, as compared to MATLAB exp(1).
-
+%
 % Output is the approximate value of e and the max k value needed to 
 % get within desired tolerance where:
 % e = sum from k = 0 to kmax (1 / k!)
